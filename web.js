7137fcb639c82647c81e6b98269a7299b5645c61
candidosales <candidosg@gmail.com>
2014-03-24 12:54:29 -0300

    ok

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var data = fs.readFileSync('index.html');
+    var buf = new Buffer(data);
+    response.send(buf.toString('utf-8'));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
